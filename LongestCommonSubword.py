329af9d7bb667461f9a8aa1f03ee90309d477aa2
Arunima Gupta <arunimagupta71@gmail.com>
2021-09-25 22:58:24 +0530

    Finding longest common subword

---
+++ LongestCommonSubword.py
@@ -0,0 +1,23 @@
+def LCW(A,B):
+    n = len(A)
+    m = len(B)
+
+    #Initializing first row and column with 0 (for ease i intialized everthing 0 :p)
+    dp = [[0 for x in range(m + 1)] for y in range(n + 1)]
+
+    final = 0
+
+    #this code is a lot like longest common subsequence(only else condition is different). 
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if A[i - 1] == B[j - 1]:
+                dp[i][j] = 1 + dp[i - 1][j - 1]
+
+            else:
+                dp[i][j] = 0
+
+            final = max(final, dp[i][j])
+
+    return final
+
+print(LCW("director","secretary"))
